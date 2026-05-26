01a3dcee97064ce70c7a6a1e6ac6511db07a83f0
dtamade <dtamade@gmail.com>
2026-05-26 15:48:31 +0800

    refactor(core): strengthen L0 foundation - base + errors
    base module (was skeletal, now production-grade):
    - Generic callback types: TProc, TProc1<T>, TFunc0<R>, TFunc1<T,R>,
      TFunc2<T1,T2,R>, TPredicate<T>
    - Generic utility types: TPair<K,V>, TComparer<T>, TEqualityCheck<T>,
      THasher<T>, THashCode
    - TByteSpan: non-owning view into byte memory (Create, FromBytes,
      Slice, bounds-checked indexing)
    - Contract assertions: Require, Ensure, CheckState, Unreachable
    - Hash functions: FNV-1a for bytes, string, integer, pointer
    
    errors module (was basic, now complete):
    - TErrorCategory enum (18 categories: InvalidArgument, Timeout,
      Cancelled, Permission, NotFound, AlreadyExists, ResourceExhausted,
      IO, Network, Parse, Internal, etc.)
    - ENextPasError with Category + Inner exception + ownership
    - 15 specific exception types, each with correct category
    - Inner exception chain support (AOwnsInner controls lifetime)

--- src/nextpas.core.base.pas
+++ src/nextpas.core.base.pas
@@ -4,6 +4,13 @@ unit nextpas.core.base;
 
 interface
 
+uses
+  SysUtils;
+
+{ ============================================================ }
+{ Framework identity                                           }
+{ ============================================================ }
+
 const
   NEXTPAS_CORE_NAME = 'nextpas.core';
   NEXTPAS_CORE_VERSION_MAJOR = 0;
@@ -11,11 +18,194 @@ const
   NEXTPAS_CORE_VERSION_PATCH = 0;
   NEXTPAS_CORE_VERSION = '0.1.0';
 
+{ ============================================================ }
+{ Canonical type aliases                                       }
+{ ============================================================ }
+
 type
-  TBytes = array of Byte;
-  TSizeUInt = SizeUInt;
-  TPtrUInt = PtrUInt;
+  THashCode = UInt32;
+
+{ ============================================================ }
+{ Generic callback types                                       }
+{ ============================================================ }
+
+type
+  TProc = reference to procedure;
+  generic TProc1<T> = reference to procedure(const A: T);
+  generic TProc2<T1, T2> = reference to procedure(const A1: T1; const A2: T2);
+  generic TFunc0<TResult> = reference to function: TResult;
+  generic TFunc1<T, TResult> = reference to function(const A: T): TResult;
+  generic TFunc2<T1, T2, TResult> = reference to function(const A1: T1; const A2: T2): TResult;
+  generic TPredicate<T> = reference to function(const A: T): Boolean;
+
+{ ============================================================ }
+{ Generic utility types                                        }
+{ ============================================================ }
+
+type
+  generic TPair<TKey, TValue> = record
+    Key: TKey;
+    Value: TValue;
+    class function Create(const AKey: TKey; const AValue: TValue): specialize TPair<TKey, TValue>; static; inline;
+  end;
+
+  generic TComparer<T> = reference to function(const A, B: T): Int32;
+  generic TEqualityCheck<T> = reference to function(const A, B: T): Boolean;
+  generic THasher<T> = reference to function(const A: T): THashCode;
+
+{ ============================================================ }
+{ Non-owning byte span (view into existing memory)             }
+{ ============================================================ }
+
+type
+  TByteSpan = record
+    Data: PByte;
+    Len: SizeUInt;
+    class function Create(const AData: PByte; const ALen: SizeUInt): TByteSpan; static; inline;
+    class function FromBytes(const ABytes: TBytes): TByteSpan; static; inline;
+    class function Empty: TByteSpan; static; inline;
+    function IsEmpty: Boolean; inline;
+    function Slice(const AOffset, ALength: SizeUInt): TByteSpan;
+    function GetByte(const AIndex: SizeUInt): Byte; inline;
+    property Items[const AIndex: SizeUInt]: Byte read GetByte; default;
+  end;
+
+{ ============================================================ }
+{ Contract assertions (design-by-contract)                     }
+{ ============================================================ }
+
+procedure Require(const ACondition: Boolean; const AMessage: string = 'precondition violated');
+procedure Ensure(const ACondition: Boolean; const AMessage: string = 'postcondition violated');
+procedure CheckState(const ACondition: Boolean; const AMessage: string = 'invalid state');
+procedure Unreachable(const AMessage: string = 'unreachable code reached');
+
+{ ============================================================ }
+{ Common hash functions                                        }
+{ ============================================================ }
+
+function HashBytes(const AData: PByte; const ALen: SizeUInt): THashCode;
+function HashString(const AValue: string): THashCode;
+function HashInteger(const AValue: Int64): THashCode;
+function HashPointer(const AValue: Pointer): THashCode;
 
 implementation
+
+{ TPair<TKey, TValue> }
+
+class function TPair.Create(const AKey: TKey; const AValue: TValue): specialize TPair<TKey, TValue>;
+begin
+  Result.Key := AKey;
+  Result.Value := AValue;
+end;
+
+{ TByteSpan }
+
+class function TByteSpan.Create(const AData: PByte; const ALen: SizeUInt): TByteSpan;
+begin
+  Result.Data := AData;
+  Result.Len := ALen;
+end;
+
+class function TByteSpan.FromBytes(const ABytes: TBytes): TByteSpan;
+begin
+  if Length(ABytes) > 0 then
+  begin
+    Result.Data := @ABytes[0];
+    Result.Len := SizeUInt(Length(ABytes));
+  end
+  else
+  begin
+    Result.Data := nil;
+    Result.Len := 0;
+  end;
+end;
+
+class function TByteSpan.Empty: TByteSpan;
+begin
+  Result.Data := nil;
+  Result.Len := 0;
+end;
+
+function TByteSpan.IsEmpty: Boolean;
+begin
+  Result := Len = 0;
+end;
+
+function TByteSpan.Slice(const AOffset, ALength: SizeUInt): TByteSpan;
+begin
+  if AOffset + ALength > Len then
+    raise ERangeError.CreateFmt('TByteSpan.Slice: offset %d + length %d > span length %d',
+      [AOffset, ALength, Len]);
+  Result.Data := Data + AOffset;
+  Result.Len := ALength;
+end;
+
+function TByteSpan.GetByte(const AIndex: SizeUInt): Byte;
+begin
+  if AIndex >= Len then
+    raise ERangeError.CreateFmt('TByteSpan: index %d out of range [0..%d]', [AIndex, Len - 1]);
+  Result := (Data + AIndex)^;
+end;
+
+{ Contract assertions }
+
+procedure Require(const ACondition: Boolean; const AMessage: string);
+begin
+  if not ACondition then
+    raise EArgumentException.Create(AMessage);
+end;
+
+procedure Ensure(const ACondition: Boolean; const AMessage: string);
+begin
+  if not ACondition then
+    raise EAssertionFailed.Create(AMessage);
+end;
+
+procedure CheckState(const ACondition: Boolean; const AMessage: string);
+begin
+  if not ACondition then
+    raise EInvalidOpException.Create(AMessage);
+end;
+
+procedure Unreachable(const AMessage: string);
+begin
+  raise EAssertionFailed.Create(AMessage);
+end;
+
+{ Hash functions - FNV-1a }
+
+const
+  FNV_OFFSET_BASIS_32 = THashCode(2166136261);
+  FNV_PRIME_32        = THashCode(16777619);
+
+function HashBytes(const AData: PByte; const ALen: SizeUInt): THashCode;
+var
+  LI: SizeUInt;
+begin
+  Result := FNV_OFFSET_BASIS_32;
+  for LI := 0 to ALen - 1 do
+  begin
+    Result := Result xor THashCode((AData + LI)^);
+    Result := Result * FNV_PRIME_32;
+  end;
+end;
+
+function HashString(const AValue: string): THashCode;
+begin
+  if Length(AValue) > 0 then
+    Result := HashBytes(@AValue[1], SizeUInt(Length(AValue)))
+  else
+    Result := FNV_OFFSET_BASIS_32;
+end;
+
+function HashInteger(const AValue: Int64): THashCode;
+begin
+  Result := HashBytes(@AValue, SizeOf(AValue));
+end;
+
+function HashPointer(const AValue: Pointer): THashCode;
+begin
+  Result := HashBytes(@AValue, SizeOf(AValue));
+end;
 
 end.
--- src/nextpas.core.errors.pas
+++ src/nextpas.core.errors.pas
@@ -5,40 +5,251 @@ unit nextpas.core.errors;
 interface
 
 uses
-  SysUtils,
-  nextpas.core.base;
+  SysUtils;
 
 type
-  { ENextPasError - framework root exception }
-  ENextPasError = class(Exception);
+  { 错误分类码 }
+  TErrorCategory = (
+    ecNone,
+    ecInvalidArgument,
+    ecNullReference,
+    ecInvalidOperation,
+    ecNotImplemented,
+    ecNotSupported,
+    ecTimeout,
+    ecCancelled,
+    ecInterrupted,
+    ecWouldBlock,
+    ecPermission,
+    ecNotFound,
+    ecAlreadyExists,
+    ecResourceExhausted,
+    ecIO,
+    ecNetwork,
+    ecParse,
+    ecInternal
+  );
 
-  { EArgumentError - invalid argument passed to a function }
-  EArgumentError = class(ENextPasError);
+  { ENextPasError - framework root exception with category + inner }
+  ENextPasError = class(Exception)
+  private
+    FCategory: TErrorCategory;
+    FInner: Exception;
+    FOwnsInner: Boolean;
+  public
+    constructor Create(const AMessage: string); overload;
+    constructor Create(const AMessage: string; const ACategory: TErrorCategory); overload;
+    constructor Create(const AMessage: string; const AInner: Exception; const AOwnsInner: Boolean = True); overload;
+    constructor Create(const AMessage: string; const ACategory: TErrorCategory; const AInner: Exception; const AOwnsInner: Boolean = True); overload;
+    destructor Destroy; override;
+    property Category: TErrorCategory read FCategory;
+    property Inner: Exception read FInner;
+  end;
 
-  { ENullReferenceError - nil dereference }
-  ENullReferenceError = class(ENextPasError);
+  { Specific exception types }
+  EArgumentError = class(ENextPasError)
+  public
+    constructor Create(const AMessage: string); overload;
+  end;
 
-  { EInvalidOperationError - operation not valid for current state }
-  EInvalidOperationError = class(ENextPasError);
+  ENullReferenceError = class(ENextPasError)
+  public
+    constructor Create(const AMessage: string); overload;
+  end;
 
-  { ENotImplementedError - feature not yet implemented }
-  ENotImplementedError = class(ENextPasError);
+  EInvalidOperationError = class(ENextPasError)
+  public
+    constructor Create(const AMessage: string); overload;
+  end;
 
-  { ENotSupportedError - operation not supported }
-  ENotSupportedError = class(ENextPasError);
+  ENotImplementedError = class(ENextPasError)
+  public
+    constructor Create(const AMessage: string); overload;
+  end;
 
-  { ETimeoutError - operation timed out }
-  ETimeoutError = class(ENextPasError);
+  ENotSupportedError = class(ENextPasError)
+  public
+    constructor Create(const AMessage: string); overload;
+  end;
 
-  { EIOError - I/O operation failed }
-  EIOError = class(ENextPasError);
+  ETimeoutError = class(ENextPasError)
+  public
+    constructor Create(const AMessage: string); overload;
+  end;
 
-  { EOutOfMemoryError - memory allocation failed }
-  EOutOfMemoryError = class(ENextPasError);
+  ECancelledError = class(ENextPasError)
+  public
+    constructor Create(const AMessage: string); overload;
+  end;
 
-  { EIndexOutOfRangeError - index out of bounds }
-  EIndexOutOfRangeError = class(ENextPasError);
+  EPermissionError = class(ENextPasError)
+  public
+    constructor Create(const AMessage: string); overload;
+  end;
+
+  ENotFoundError = class(ENextPasError)
+  public
+    constructor Create(const AMessage: string); overload;
+  end;
+
+  EAlreadyExistsError = class(ENextPasError)
+  public
+    constructor Create(const AMessage: string); overload;
+  end;
+
+  EResourceExhaustedError = class(ENextPasError)
+  public
+    constructor Create(const AMessage: string); overload;
+  end;
+
+  EIOError = class(ENextPasError)
+  public
+    constructor Create(const AMessage: string); overload;
+  end;
+
+  ENetworkError = class(ENextPasError)
+  public
+    constructor Create(const AMessage: string); overload;
+  end;
+
+  EParseError = class(ENextPasError)
+  public
+    constructor Create(const AMessage: string); overload;
+  end;
+
+  EIndexOutOfRangeError = class(ENextPasError)
+  public
+    constructor Create(const AMessage: string); overload;
+  end;
+
+  EOutOfMemoryError = class(ENextPasError)
+  public
+    constructor Create(const AMessage: string); overload;
+  end;
 
 implementation
+
+{ ENextPasError }
+
+constructor ENextPasError.Create(const AMessage: string);
+begin
+  inherited Create(AMessage);
+  FCategory := ecNone;
+  FInner := nil;
+  FOwnsInner := False;
+end;
+
+constructor ENextPasError.Create(const AMessage: string; const ACategory: TErrorCategory);
+begin
+  inherited Create(AMessage);
+  FCategory := ACategory;
+  FInner := nil;
+  FOwnsInner := False;
+end;
+
+constructor ENextPasError.Create(const AMessage: string; const AInner: Exception; const AOwnsInner: Boolean);
+begin
+  inherited Create(AMessage);
+  FCategory := ecNone;
+  FInner := AInner;
+  FOwnsInner := AOwnsInner;
+end;
+
+constructor ENextPasError.Create(const AMessage: string; const ACategory: TErrorCategory; const AInner: Exception; const AOwnsInner: Boolean);
+begin
+  inherited Create(AMessage);
+  FCategory := ACategory;
+  FInner := AInner;
+  FOwnsInner := AOwnsInner;
+end;
+
+destructor ENextPasError.Destroy;
+begin
+  if FOwnsInner and (FInner <> nil) then
+    FInner.Free;
+  inherited;
+end;
+
+{ Specific exceptions }
+
+constructor EArgumentError.Create(const AMessage: string);
+begin
+  inherited Create(AMessage, ecInvalidArgument);
+end;
+
+constructor ENullReferenceError.Create(const AMessage: string);
+begin
+  inherited Create(AMessage, ecNullReference);
+end;
+
+constructor EInvalidOperationError.Create(const AMessage: string);
+begin
+  inherited Create(AMessage, ecInvalidOperation);
+end;
+
+constructor ENotImplementedError.Create(const AMessage: string);
+begin
+  inherited Create(AMessage, ecNotImplemented);
+end;
+
+constructor ENotSupportedError.Create(const AMessage: string);
+begin
+  inherited Create(AMessage, ecNotSupported);
+end;
+
+constructor ETimeoutError.Create(const AMessage: string);
+begin
+  inherited Create(AMessage, ecTimeout);
+end;
+
+constructor ECancelledError.Create(const AMessage: string);
+begin
+  inherited Create(AMessage, ecCancelled);
+end;
+
+constructor EPermissionError.Create(const AMessage: string);
+begin
+  inherited Create(AMessage, ecPermission);
+end;
+
+constructor ENotFoundError.Create(const AMessage: string);
+begin
+  inherited Create(AMessage, ecNotFound);
+end;
+
+constructor EAlreadyExistsError.Create(const AMessage: string);
+begin
+  inherited Create(AMessage, ecAlreadyExists);
+end;
+
+constructor EResourceExhaustedError.Create(const AMessage: string);
+begin
+  inherited Create(AMessage, ecResourceExhausted);
+end;
+
+constructor EIOError.Create(const AMessage: string);
+begin
+  inherited Create(AMessage, ecIO);
+end;
+
+constructor ENetworkError.Create(const AMessage: string);
+begin
+  inherited Create(AMessage, ecNetwork);
+end;
+
+constructor EParseError.Create(const AMessage: string);
+begin
+  inherited Create(AMessage, ecParse);
+end;
+
+constructor EIndexOutOfRangeError.Create(const AMessage: string);
+begin
+  inherited Create(AMessage, ecInvalidArgument);
+end;
+
+constructor EOutOfMemoryError.Create(const AMessage: string);
+begin
+  inherited Create(AMessage, ecResourceExhausted);
+end;
 
 end.
